7554945fad50a867343c72c6ed0af035095f4c54
Ashwin Kumar <ashwinkumar2468@gmail.com>
2021-12-15 17:59:07 -0500

    update alertPage to follow MVC

--- project1/src/edu/neu/csye6200/Driver.java
+++ project1/src/edu/neu/csye6200/Driver.java
@@ -1,36 +1,22 @@
 package edu.neu.csye6200;
 
-import java.awt.Color;
 import java.awt.EventQueue;
-import java.awt.SystemColor;
 import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
 import edu.neu.csye6200.controllers.AlertController;
+import edu.neu.csye6200.controllers.InfoController;
 import edu.neu.csye6200.controllers.RegistrationController;
 import edu.neu.csye6200.controllers.StudentImmunizationController;
 import edu.neu.csye6200.models.Student;
 import edu.neu.csye6200.models.Teacher;
 import edu.neu.csye6200.views.AlertView;
+import edu.neu.csye6200.views.InfoView;
 import edu.neu.csye6200.views.RegistrationView;
 import edu.neu.csye6200.views.StudentImmunizationView;
-import net.proteanit.sql.DbUtils;
-
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.awt.event.ActionEvent;
 import javax.swing.JTable;
-import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.SwingConstants;
-import java.awt.Dimension;
-import javax.swing.border.LineBorder;
-import javax.swing.JScrollPane;
 
 public class Driver {
 
@@ -38,6 +24,7 @@ public class Driver {
 	StudentImmunizationView studentImmunizationView;
 	RegistrationView registrationView;
 	AlertView alertView;
+	InfoView infoView;
 	private JTable table;
 
 	/**
@@ -77,6 +64,7 @@ public class Driver {
 		registrationView = new RegistrationView();
 		studentImmunizationView = new StudentImmunizationView();
 		alertView = new AlertView();
+		infoView = new InfoView();
 		generateHomePage();
 
 		// init controllers
@@ -84,6 +72,7 @@ public class Driver {
 		StudentImmunizationController studentImmunizationController = new StudentImmunizationController(studentModel,
 				studentImmunizationView);
 		AlertController alertController = new AlertController(studentModel, alertView);
+		InfoController infoController = new InfoController(studentModel, infoView);
 	}
 
 	/**
@@ -108,72 +97,7 @@ public class Driver {
 		homeTabbedPane.addTab("Student Immunization", null, StudentImmunizationPage, null);
 
 		// 3. Information Page
-		JPanel infoPanel = new JPanel();		
-		infoPanel.setLayout(null);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setEnabled(false);
-		scrollPane.setVerifyInputWhenFocusTarget(false);
-		scrollPane.setFont(new Font("Segoe UI Light", Font.PLAIN, 25));
-		scrollPane.setBounds(415, 100, 950, 600);
-		infoPanel.add(scrollPane);
-		
-		table = new JTable();
-		scrollPane.setViewportView(table);
-		table.setBackground(SystemColor.window);
-		
-		JLabel lblNewLabel = new JLabel("Patrons Information Page");
-		lblNewLabel.setMinimumSize(new Dimension(155, 13));
-		lblNewLabel.setMaximumSize(new Dimension(155, 13));
-		lblNewLabel.setPreferredSize(new Dimension(165, 13));
-		lblNewLabel.setOpaque(true);
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBackground(SystemColor.activeCaption);
-		lblNewLabel.setFont(new Font("Segoe UI Light", Font.PLAIN, 30));
-		lblNewLabel.setBounds(415, 23, 950, 53);
-		infoPanel.add(lblNewLabel);
-		
-		JButton btnNewButton_1 = new JButton("Student Details");
-		btnNewButton_1.setForeground(SystemColor.window);
-		btnNewButton_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Statement statmentShowStudentList;
-				try {
-					statmentShowStudentList = a.createStatement();
-					ResultSet rs = statmentShowStudentList.executeQuery("select * from student");
-					table.setModel(DbUtils.resultSetToTableModel(rs));
-				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
-		});
-		btnNewButton_1.setBorder(new LineBorder(SystemColor.scrollbar, 1, true));
-		btnNewButton_1.setBackground(new Color(135, 206, 235));
-		btnNewButton_1.setFont(new Font("Segoe UI Light", Font.PLAIN, 25));
-		btnNewButton_1.setBounds(111, 300, 182, 60);
-		infoPanel.add(btnNewButton_1);
-		
-		JButton btnNewButton_1_1 = new JButton("Teachers Details");
-		btnNewButton_1_1.setForeground(SystemColor.window);
-		btnNewButton_1_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Statement statmentShowStudentList;
-				try {
-					statmentShowStudentList = a.createStatement();
-					ResultSet rs = statmentShowStudentList.executeQuery("select * from teacher");
-					table.setModel(DbUtils.resultSetToTableModel(rs));
-				} catch (SQLException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
-		});
-		btnNewButton_1_1.setFont(new Font("Segoe UI Light", Font.PLAIN, 25));
-		btnNewButton_1_1.setBorder(new LineBorder(SystemColor.scrollbar, 1, true));
-		btnNewButton_1_1.setBackground(new Color(135, 206, 235));
-		btnNewButton_1_1.setBounds(111, 400, 182, 60);
-		infoPanel.add(btnNewButton_1_1);
+		JPanel infoPanel = infoView.initialize();
 		homeTabbedPane.addTab("Information", null, infoPanel, null);
 
 		// 4. Alerts Page
